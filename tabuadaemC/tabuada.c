5a1d4b862e2fe3d0265df4ca86a3b977bda83423
Mateus Gomes <127147447+omattaeus@users.noreply.github.com>
2023-04-19 20:25:12 -0300

    criei projeto

---
+++ tabuadaemC/tabuada.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int numero, cont=0;
+
+    printf("Digite um numero da tabuada: ");
+    scanf("%d",&numero);
+
+    for (cont=0; cont<=10 ; cont++){
+        printf("%d x %d = %d \n",numero,cont,numero*cont);
+    }
+    return 0;
+}
